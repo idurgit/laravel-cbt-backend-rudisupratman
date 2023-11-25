13bb7887d3fc9bf6da7fc09cb46ab177482ca78b
Rudi <126459042+idurgit@users.noreply.github.com>
2023-11-25 23:10:51 +0700

    laravel api

---
+++ app/Http/Controllers/Api/UjianController.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\SoalResource;
+use App\Models\Soal;
+use App\Models\Ujian;
+use App\Models\UjianSoalList;
+use Illuminate\Http\Request;
+
+class UjianController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+
+    public function createUjian(Request $request)
+    {
+        $soalAngka = Soal::where('kategori', 'Numeric')->inRandomOrder()->limit(20)->get();
+        $soalVerbal = Soal::where('kategori', 'Verbal')->inRandomOrder()->limit(20)->get();
+        $soalLogika = Soal::where('kategori', 'Logika')->inRandomOrder()->limit(20)->get();
+
+        $ujian = Ujian::create([
+            'user_id' => $request->user()->id,
+        ]);
+
+        foreach ($soalAngka as $soal) {
+            UjianSoalList::create([
+                'ujian_id' => $ujian->id,
+                'soal_id' => $soal->id,
+            ]);
+        }
+
+        foreach ($soalVerbal as $soal) {
+            UjianSoalList::create([
+                'ujian_id' => $ujian->id,
+                'soal_id' => $soal->id,
+            ]);
+        }
+
+        foreach ($soalLogika as $soal) {
+            UjianSoalList::create([
+                'ujian_id' => $ujian->id,
+                'soal_id' => $soal->id,
+            ]);
+        }
+
+        return response()->json([
+            'message' => 'Ujian berhasil dibuat',
+            'data' => $ujian,
+        ]);
+    }
+
+    public function getListSoalbyKategori(Request $request)
+    {
+        $ujian = Ujian::where('user_id', $request->user()->id)->first();
+        $ujianSoalList = UjianSoalList::where('ujian_id', $ujian->id)->get();
+        $soalIds = $ujianSoalList->pluck('soal_id');
+        
+        $soal = Soal::whereIn('id', $soalIds)->where('kategori', $request->kategori)->get();
+
+        return response()->json([
+            'message' => 'Berhasil mendapatkan soal',
+            'data' => SoalResource::collection($soal),
+        ]);
+    }
+
+    public function jawabSoal(Request $request)
+    {
+        $validatedData = $request->validate([
+            'soal_id' => 'required',
+            'jawaban' => 'required',
+        ]);
+
+        $ujian = Ujian::where('user_id', $request->user()->id)->first();
+        $ujianSoalList = UjianSoalList::where('ujian_id', $ujian->id)->where('soal_id', $validatedData['soal_id'])->first();
+        $soal = Soal::where('id', $validatedData['soal_id'])->first();
+
+        if ($soal->kunci == $validatedData['jawaban']) {
+            // $ujianSoalList->kebenaran = true;
+            $ujianSoalList->update([
+                'kebenaran' => true
+            ]);
+        } else {
+            // $ujianSoalList->kebenaran = false;
+            $ujianSoalList->update([
+                'kebenaran' => false
+            ]);
+        }
+
+        return response()->json([
+            'message' => 'Berhasil simpan jawaban',
+            'jawaban' => $ujianSoalList->kebenaran,
+        ]);
+    }
+}
